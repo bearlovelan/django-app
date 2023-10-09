0a6e963cc8f1c156c21f103bbf134a8ee0a82ac9
bearlovelan <18987934743@163.com>
2023-10-09 21:33:27 +0800

    modify allowed host

--- xjeapp/settings.py
+++ xjeapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-dw76up0_=ec%8!no3za3eb4es^v-lkvvp@ws)zi8n3&709(cn)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["111.230.49.81", "localhost"]
 
 
 # Application definition
